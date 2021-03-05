5d8cdf78287028a628b449d08dab0715ead5b841
Erin Moore <erinmoorethefirst@gmail.com>
2021-03-05 17:56:03 -0500

    date fixes

--- get_the_tweets.sh
+++ get_the_tweets.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-START_DATE=$(tail dates.txt)
-TODAY_DATE=$(date +'%Y-%m-%d')
+START_DATE=$(tail -n1 dates.txt)
+END_DATE=$(date +'%Y-%m-%d')
+FILE_NAME="tweets/${START_DATE}_${END_DATE}.txt"
 
-twint -u tacoblaseball --since $START_DATE -o "tweets/${START_DATE}_${TODAY_DATE}.txt"
-echo "${TODAY_DATE}" >> dates.txt
+twint -u tacoblaseball --since $START_DATE -o $FILE_NAME
+date -d "${END_DATE} 1 days" +%Y-%m-%d >> dates.txt
 
-sed -i 's/.*<tacoblaseball>//g' "tweets/${START_DATE}_${TODAY_DATE}.txt"
+sed -i 's/.*<tacoblaseball>//g' $FILE_NAME
 
 #.*<tacoblaseball>
 #date +'%Y-%m-%d'
