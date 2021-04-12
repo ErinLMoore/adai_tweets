9c10d9e850ea18353071a4856f72f7b50dd6f9b3
Erin Moore <erinmoorethefirst@gmail.com>
2021-04-12 15:54:01 -0400

    preprocesses text more

--- get_the_tweets.sh
+++ get_the_tweets.sh
@@ -8,7 +8,13 @@ twint -u tacoblaseball --since $START_DATE -o $FILE_NAME
 date -d "${END_DATE} 1 days" +%Y-%m-%d >> dates.txt
 
 sed -i 's/.*<tacoblaseball>//g' $FILE_NAME
-sed -i -e 'G;' $FILE_NAME
 
-#.*<tacoblaseball>
-#date +'%Y-%m-%d'
+rg -i --passthru '^ (@\w+\s)*(ooc.+)?' -r '' $FILE_NAME | sponge $FILE_NAME
+rg -i --passthru 'https://\S+\b' -r '' $FILE_NAME | sponge $FILE_NAME
+rg -U --passthru  '(\r?\n)+ ' -r '\n\n' $FILE_NAME | sponge $FILE_NAME
+rg --passthru  "&gt;" -r '>' $FILE_NAME | sponge $FILE_NAME
+rg --passthru  "&lt;" -r '<' $FILE_NAME | sponge $FILE_NAME
+rg -U --passthru  '\n{2,}' -r '\n\n' $FILE_NAME | sponge $FILE_NAME
+
+sed -i '/^$/d' $FILE_NAME
+sed -i -e 'G;' $FILE_NAME
